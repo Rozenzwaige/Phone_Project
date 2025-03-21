3131823884b2ce4defadad00e885cd3177102415
Rozenzwaige <78435898+Rozenzwaige@users.noreply.github.com>
2025-03-21 11:04:55 +0200

    config.py

--- config.py
+++ config.py
@@ -2,14 +2,15 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    SECRET_FILE = os.getenv("SECRET_FILE")
-    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-    GOOGLE_CLIENT_SECRET = None
-    AUTHORIZED_EMAILS = ["nroznim@gmail.com", "naamakunik@gmail.com"]
-
-    # קריאת GOOGLE_CLIENT_SECRET מתוך הקובץ ב-Render או ממשתנה סביבה
-    if SECRET_FILE and os.path.exists(SECRET_FILE):
-        with open(SECRET_FILE, "r") as file:
+    
+    # קריאת GOOGLE_CLIENT_SECRET מתוך קובץ ב-Render או ממשתנה סביבה
+    SECRET_FILE_PATH = os.getenv("SECRET_FILE")
+    if SECRET_FILE_PATH and os.path.exists(SECRET_FILE_PATH):
+        with open(SECRET_FILE_PATH, "r") as file:
             GOOGLE_CLIENT_SECRET = file.read().strip()
     else:
         GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    
+    AUTHORIZED_EMAILS = ['nroznim@gmail.com', 'naamakunik@gmail.com']
