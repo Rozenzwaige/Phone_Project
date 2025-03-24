be4e0c1354a897ce7f9929a5417db9da32b1a614
Rozenzwaige <78435898+Rozenzwaige@users.noreply.github.com>
2025-03-24 11:40:48 +0200

    app.py

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ google = oauth.register(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     userinfo_endpoint='https://www.googleapis.com/oauth2/v1/userinfo',
-    jwks_uri='https://www.googleapis.com/oauth2/v3/certs'
+    jwks_uri='https://www.googleapis.com/oauth2/v3/certs',
     client_kwargs={
     'scope': 'openid email profile',
     'prompt': 'consent'
 }
-
+server_metadata_url="https://accounts.google.com/.well-known/openid-configuration"
 )
 
 @app.route('/')
