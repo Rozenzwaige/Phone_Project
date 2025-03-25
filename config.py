e6ab40294e6335a3bdffb86a55a902efbbe2fae0
Rozenzwaige <78435898+Rozenzwaige@users.noreply.github.com>
2025-03-25 11:43:49 +0200

    config.py

--- config.py
+++ config.py
@@ -1,19 +1,6 @@
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///site.db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
