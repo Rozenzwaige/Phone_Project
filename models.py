5870d3e52dfc7fbe4014b34e394dc5a831c6407f
Rozenzwaige <78435898+Rozenzwaige@users.noreply.github.com>
2025-03-25 11:54:50 +0200

    models.py

--- models.py
+++ models.py
@@ -1,12 +1,9 @@
-from app import db, login_manager
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    is_approved = db.Column(db.Boolean, default=False)
+    is_approved = db.Column(db.Boolean, default=False)  # שדה שמסמן האם המשתמש מאושר
